b61fac58f1e068d35fa5c91656edfc1aa73ecc08
tompongfitz <94457181+tompongfitz@users.noreply.github.com>
2026-03-23 17:46:49 +0800

    Update R4.ino

--- R4.ino
+++ R4.ino
@@ -1,26 +1,12 @@
-/* PAGTULTOL TRANSMITTER (ARDUINO UNO R4 WIFI)
-   - Reads DS18B20
-   - Sends LoRa (SF12)
-   - Communicates with Python App via Serial
-   - NO CLOUD / OFFLINE MODE
-*/
-
 #include <SPI.h>
 #include <LoRa.h>
-//#include <OneWire.h>
-//#include <DallasTemperature.h>
 #include "BP.h"
 #include "Bonezegei_ULN2003_Stepper.h"
 
-// ---------------------------------------------------------------------------  
-//  HARDWARE CONFIGURATION
-// ---------------------------------------------------------------------------
-//#define ONE_WIRE_BUS 4    // DS18B20 Pin
-#define TX_LED_PIN   5    // Monitoring LED
+#define TX_LED_PIN   5
 #define BP_START_PIN 8
 #define BUZZER 7
 
-// LoRa Pins for Arduino UNO R4
 #define LORA_SS      10
 #define LORA_RST     9
 #define LORA_DIO0    2
@@ -34,33 +20,13 @@ bool isAlarmActive = false;
 unsigned long previousBuzzerMillis = 0;
 bool buzzerState = LOW;
 
-
-// ---------------------------------------------------------------------------
-//  INITIALIZATION
-// ---------------------------------------------------------------------------
-// Initialize Hardware
-//OneWire oneWire(ONE_WIRE_BUS);
-//DallasTemperature sensors(&oneWire);
-
-
-//void sendLoRaMessage(String message) {
-//  LoRa.beginPacket();
-//  LoRa.print(message);
-//  if (LoRa.endPacket() == 0) {
-//    // Silent fail
-//  }
-//}
-
 void setup() {
-  // 1. Initialize Serial (Communication with Python)
   Stepper.begin();
   Stepper.setSpeed(5);
   
   Serial.begin(9600);
   delay(500);
 
-
-  // 2. Initialize Hardware
   pinMode(BUZZER, OUTPUT);
   digitalWrite(BUZZER, LOW);
   pinMode(BP_START_PIN, OUTPUT);  
@@ -68,49 +34,29 @@ void setup() {
   digitalWrite(BP_START_PIN, HIGH);
   digitalWrite(TX_LED_PIN, LOW);
   
-  //sensors.begin();
-  BP_init(); // Assuming this is defined in your "BP.h"
+  BP_init(); 
   delay(1500);
 
-
-  // 3. Initialize LoRa
   LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
- 
+  
   if (!LoRa.begin(433E6)) {
     Serial.println("LoRa Init Failed!");
   } 
   else {
-    // --- PRIVACY & RANGE SETTINGS ---
-    LoRa.setSyncWord(0xF3);          // PRIVATE KEY: Must match Receiver!
+    LoRa.setSyncWord(0xF3);
     
-    LoRa.setTxPower(20);             // Max Power
-    LoRa.setSignalBandwidth(50E3);   // Match Receiver
-    LoRa.setSpreadingFactor(12);     // Match Receiver
-    LoRa.setCodingRate4(8);          // Match Receiver
+    LoRa.setTxPower(20);
+    LoRa.setSignalBandwidth(50E3);
+    LoRa.setSpreadingFactor(12);
+    LoRa.setCodingRate4(8);
     LoRa.enableCrc();
   }
 }
 
-
-
-
 void loop() {
-  // 1. Read Temperature
-  //sensors.requestTemperatures();
-//  float tempC = sensors.getTempCByIndex(0);
-
-  // Check valid reading (DS18B20 returns -127 if error)
-//  if(tempC > -100 && tempC < 100) {
-    
-    // --- SEND TO PYTHON APP (USB) ---
-//    Serial.println(tempC);
-
-    // --- CHECK FOR PYTHON COMMANDS ---
-
     int s = bp_sys;
     int d = bp_dia;
     int p = bp_bpm;
-    
     
     if (Serial.available() > 0) {
       String command = Serial.readStringUntil('\n');
@@ -135,47 +81,4 @@ void loop() {
         LoRa.print("START_SCAN");
         LoRa.endPacket();
         digitalWrite(TX_LED_PIN, HIGH);
-        digitalWrite(BP_START_PIN, LOW);
-        delay(200);
-        digitalWrite(BP_START_PIN, HIGH);
-        
-      }
-      else if (command == "STOP") {
-        LoRa.beginPacket();
-        LoRa.print("STOP_SCAN");
-        LoRa.endPacket();
-        digitalWrite(TX_LED_PIN, LOW);
-        digitalWrite(BP_START_PIN, LOW);
-        delay(200);
-        digitalWrite(BP_START_PIN, HIGH);
-        }
-      else if (command == "BEEP") {
-        digitalWrite(7, HIGH);
-        delay(500);
-        digitalWrite(7, LOW);
-        delay(500);
-        }
-      else if (command == "ALARM_ON") {
-        isAlarmActive = true;
-        }
-      else if (command == "ALARM_OFF") {
-        isAlarmActive = false;
-        digitalWrite(BUZZER, LOW);
-        }
-      else if (command == "ROTATE")
-        Stepper.step(REVERSE, 127);
-      }
-    if (isAlarmActive) {
-      unsigned long currentMillis = millis();
-      if (currentMillis - previousBuzzerMillis >= 1000) { // 1000 ms = 1 second
-        previousBuzzerMillis = currentMillis;
-        buzzerState = !buzzerState; // Toggle state between HIGH and LOW
-        digitalWrite(BUZZER, buzzerState);
-      }
-    } else {
-      digitalWrite(BUZZER, LOW);
-    }
-      
-
-   BP_getData(); 
-}
+        digitalWrite(
